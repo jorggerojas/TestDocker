ace46bd9c9d866fcf7564d144da91a4bb7bf3b5b
Jorge Luis Rojas Arcos <jorgelroar@gmail.com>
2021-01-11 18:21:33 -0600

    feature: create dog file and import into app

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const user = require("./user");
+const Dog = require("./dog");
 
 const app = express();
+const todd = new Dog("Todd", 2, "Labrador", "golden");
+const tobby = new Dog("Tobby", 2, "Chow chow", "black");
 
 app.get("/", (_, res) => {
   res.json({ response: "OK" });
 });
+console.log(todd.present());
+console.log(tobby.present());
 console.log(1 + 1);
 console.log(JSON.stringify(user));
 console.log(user);
